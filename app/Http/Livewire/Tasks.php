c5e6661db07ee15fbb7e6380b892528d8919b5d8
EBirodar97 <92210441+EBirodar@users.noreply.github.com>
2022-09-09 18:10:07 +0500

    task fixed

--- app/Http/Livewire/Tasks.php
+++ app/Http/Livewire/Tasks.php
@@ -7,44 +7,53 @@ use Livewire\Component;
 class Tasks extends Component
 {
     public $amount=1000;
+    public $newbalance;
     public $oldamount=1000;
     public $balance;
-    public $newAmountUsd=0;
-    public $newAmountUzs=0;
-    public $newAmountEuro=0;
+    public $currencyType=1;
     public $usd=1;
     public $usz=11000;
+    public $currencyy = [];
     public $euro=1.1;
     public $currencies=[
         [
             'id'=>1,
             'name'=>'USD',
-            'values'=>1
+            'value'=>1
         ],
         [
             'id'=>2,
             'name'=>'UZS',
-            'values'=>11000
+            'value'=>11000
         ],
         [
             'id'=>3,
             'name'=>'EURO',
-            'values'=>1.1
+            'value'=>1.1
         ],
 
     ];
 
 
-    public function update()
+    public function updated()
     {
 
-        $this->balance=$this->amount-$this->newAmountUsd;
+        $this->balance=$this->amount;
+        if(isset($this->currencyy)){
+            foreach ($this->currencyy as $key=>$item){
+
+                $x=$this->currencies[$key-1]['value'];
+                if($item==null){
+                    $item=0;
+                }
+                $this->newbalance=$item*$this->currencyType/$x;
+                $this->balance-=$this->newbalance;
+            }
+            $this->balance=$this->balance/$this->currencyType;
+        }
 
     }
-    public function mount()
-    {
-        $this->balance=$this->amount-$this->newAmountUsd;
-    }
+
     public function render()
     {
         return view('livewire.tasks');
--- resources/views/livewire/tasks.blade.php
+++ resources/views/livewire/tasks.blade.php
@@ -6,9 +6,9 @@
                     <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
                         Course Currency
                     </label>
-                    <select name="currency" id="currency">
+                    <select  id="currency" wire:model="currencyType" >
                         @foreach($currencies as $currency)
-                        <option value="{{$currency->value}}" >{{$currency->name}}</option>
+                        <option value="{{$currency['value']}}" >{{$currency['name']}}</option>
                         @endforeach
                     </select>
                 </div>
@@ -22,18 +22,25 @@
             </div>
         </div>
         <div class="flex flex-wrap -mx-3 mb-6">
-            @foreach($currencies as $key=>$currency)
+            @foreach($currencies as $currency)
             <div class="w-full row px-3 my-3">
-                <h1 class="col-4 d-flex align-items-center">{{$currency->name}}</h1>
-                <h2 class="col-4 d-flex align-items-center"   >{{($balance)*$currency->value}}</h2>
-                <input type="number" class="col-4" wire:model.lazy="new.{{$key}}">
+                <h1 data-field="name" class="col-4 d-flex align-items-center">{{$currency['name']}}</h1>
+                @php $id=$currency['id'] @endphp
+                <h2 class="col-4 d-flex align-items-center"   >
+                    {{round($balance*$currency['value'],5)}}
+                </h2>
+                <input type="number" class="col-4" value="1" wire:model="currencyy.{{ $currency['id']}}">
             </div>
-{{--                <h1>{{$new.key}}</h1>--}}
             @endforeach
         <div class="flex justify-between">
             <h2>{{$amount}}</h2>
 {{--            <h2>{{$newAmountUsd+$newAmountUzs/11000+$newAmountEuro/1.1}}</h2>--}}
         </div>
+
+            @foreach($currencyy as $ass)
+                {{ $ass }}
+            @endforeach
+        {{$newbalance}}
 
     </form>
 </div>
